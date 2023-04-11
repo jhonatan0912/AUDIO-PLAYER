b799a3776ae1a28193dc01de58ba200b912f8296
Jhonatan Yaranga <jhoonatan_0912@hotmail.com>
2023-04-10 19:39:55 -0500

    data added

---
+++ src/data.ts
@@ -0,0 +1,303 @@
+import { Song } from "./app/models/Song.interface";
+
+export const data: Song[] = [
+  {
+    image: 'https://i1.sndcdn.com/artworks-000387420762-j7ora1-t500x500.jpg',
+    title: 'Honest',
+    artist: 'The Chainsmokers',
+    album: 'Honest',
+    url: '/assets/honest.mp3',
+    lyrics: `
+    The other one is, of course, outing yourself<br>
+    I think, be the first to out yourself, I always think 
+    Because you know, there's no end to the hypocrisy of the human heart 
+    Woah-woah, woah 
+    Woah-woah, woah 
+    Woah-woah, woah 
+    Woah-woah, woah 
+    It's 5:00 a.m. and I'm on the radio 
+    I'm supposed to call you, but
+    I don't know what to say at all
+    And there's this girl, she wants me to take her home
+    She don't really love me though, I'm just on the radio
+    And I'm not gonna tell you that I'm over it
+    'Cause I think about it every night I'm not sober, and
+    I know I keep these feelings to myself
+    Like I don't need nobody else
+    But you're not the only one on my mind
+    If I'm being honest
+    If I'm being honest
+    You said I should be honest
+    So I'm being honest
+    It's 6:00 a.m., I'm so far away from you
+    I don't wanna let you down
+    What am I supposed to do?
+    It's been three weeks at least, now, since I've been gone
+    And I don't even like the road
+    I'm just on the radio
+    And I'm not gonna tell you that I'm over it
+    'Cause I think about it every night I'm not sober, and
+    I know I keep these feelings to myself
+    Like I don't need nobody else
+    But you're not the only one on my mind
+    If I'm being honest
+    If I'm being honest
+    You said I should be honest
+    So I'm being honest
+    Woah-woah, woah
+    Woah-woah, woah
+    Woah-woah, woah
+    Woah-woah, woah
+    Woah-woah, woah
+    Woah-woah, woah
+    Woah-woah, woah
+    Woah-woah, woah
+    And I'm not gonna tell you that I'm over it
+    'Cause I think about it every night I'm not sober, and
+    I know I keep these feelings to myself
+    Like I don't need nobody else
+    But you're not the only one on my mind
+    If I'm being honest
+    If I'm being honest
+    You said I should be honest
+    So I'm being honest
+    If I'm being honest
+  `
+  },
+  {
+    image: "https://indierocks.b-cdn.net/wp-content/uploads/2021/11/WOS_Oscuro-%C3%89xtasis_portada.jpg",
+    title: "Mirá mamá",
+    artist: "WOS",
+    album: "Oscuro éxtasis",
+    url: "/assets/mira-mama.mp3",
+    lyrics: `
+Yo me vi largar las palabras, como un aguacero
+Pensamientos vienen muchos, solo algunos son sincero'
+¿Qué voy a hacer si nunca me basé en los cero'?
+Tengo un nudo en la garganta y en la panza un agujero
+Sé que todo esto es pasajero
+La agonía del enfermo, el amor en el verano
+Su calor en el invierno
+Pero la veo y caigo en la trampa de sentir que hay algo eterno
+"¿Cómo estás?" Le contesto que no sé llorar
+Por eso dibujo lágrimas
+Ya se humedeció toda la página, así que imaginá
+Al menos no soy una máquina
+¿Será que no quiero que esto muera y que se olviden de mí?
+Si te soy sincero
+¿Será que no quiero darme cuenta que los que les creí
+No eran tan sincero'?
+Mirá mamá, estoy arriba
+Y te juro que no hay nada más que la vida
+Mirame acá, estoy arriba
+Y te juro que no hay nada más que la vida
+Mirá mamá, 'toy arriba
+Y te juro que no hay nada más que la vida
+Mirá mamá, estoy arriba
+Y te juro que no hay nada más
+Todo se deshace y quiero irme
+Cuando no puedo pisar firme en el mismo lugar que antes
+Pero todo son instante' y escapar no sirve
+Para buscar ser libre nunca se hace tarde
+Y ahora entendí
+Que no todo está bien pero está bien así
+Una temporada de poner énfasis
+En entender el lado oscuro de este éxtasis
+Y no es que sea frágil
+Es que tengo la nostalgia con el "Sí" muy fácil
+Sé que es difícil 'tar al lado mío
+Les da náuseas el vaivén de lo que vivo
+Te preguntabas por qué estando mojado me río
+Es que me empapa este rocío compuesto por recuerdos de crío
+En un banco rimando con frío
+Amando a los mío', tomando y tentando al olvido
+Y ahora dudo si eso tuyo era amor de verdad
+O buscabas un muñeco pa' mostrar
+Y no funcionó conmigo
+No soy un placebo diseñado pa' engañar tu vacío
+¿Será que no quiero que esto muera y que se olviden de mí?
+Si te soy sincero
+¿Será que no quiero darme cuenta que los que les creí
+No eran tan sincero'?
+Mirá mamá, estoy arriba
+Y te juro que no hay nada más que la vida
+Mirame acá, estoy arriba
+Y te juro que no hay nada más que la vida
+Mirá mamá, 'toy arriba
+Y te juro que no hay nada más que la vida
+Mirá mamá, estoy arriba
+Y te juro que no hay nada más, eh
+Mirá mamá, 'toy arriba
+Y te juro que no hay nada más que la vida
+Mira mamá, estoy arriba
+Y te juro que no hay nada más
+    `
+  },
+  {
+    image: 'https://i1.sndcdn.com/artworks-000415233780-bcyvx7-t500x500.jpg',
+    title: 'Andromeda',
+    artist: 'WOS',
+    album: 'ANDRÓMEDA',
+    url: '/assets/andromeda-wos.mp3',
+    lyrics: `
+    
+    `
+  },
+  {
+    image: 'https://loqueva.com/wp-content/uploads/2019/03/Wos-Terraza-Picante-loqueva-home.jpg',
+    title: 'Terraza',
+    artist: 'Wos',
+    album: 'Terraza',
+    url: '/assets/terraza-wos.mp3',
+    lyrics: `
+ Me gusta todo lo que sos y un poco más
+El barrio queda sin luz cuando no estás
+Las caras cambian todas cuando vos llegás
+La noche te sigue mientras vos girás
+Acá todos mueren por verte fumar
+Tu magia está cerca y protege el lugar
+Ahí voy a querer estar
+Donde tu risa me saca a bailar
+Cada vez que yo no me encuentro en mí, eh
+Voy a vos para revivir, eh
+Yo voy a estar ahí, yeh
+Siempre podés venir
+Ella camina con su viajero
+Entre desastres y prisioneros
+Cuando el destino se pone austero
+Sale al rescate lo verdadero
+Ella camina con su viajero
+Entre desastres y prisioneros
+Cuando el destino se pone austero
+Sale al rescate lo verdadero
+Comprensión bendita, un par de dilemas
+Humanidad maldita, cuerpo que tirita
+La mirada que desvela, de esas que te hielan
+Gratitud finita, noches sin estrellas o con miles de ellas
+Alma dinamita, voces en la cripta
+Felicidad compleja, una espera escrita
+Angustias bellas, onomatopeya
+Río que se agita, una luz interna
+Fuego que habilita lágrima que quema
+Protección eterna, un amor que grita
+Cada vez que yo no me encuentro en mí, eh
+Voy a vos para revivir, eh
+Yo voy a estar ahí, yeh
+Siempre podés venir
+Ella camina con su viajero
+Entre desastres y prisioneros
+Cuando el destino se pone austero
+Sale al rescate lo verdadero
+Ella camina con su viajero
+Entre desastres y prisioneros
+Cuando el destino se pone austero
+Sale al rescate lo verdadero
+    `
+  },
+  {
+    image: 'https://i1.sndcdn.com/artworks-Ipy9cNUrIJz5ApLQ-uZrrAg-t500x500.jpg',
+    title: 'Alma dinamita',
+    artist: 'WOS',
+    album: 'Tres puntos suspensivos',
+    url: '/assets/alma-dinamita-wos.mp3',
+    lyrics: `
+ Me gusta todo lo que sos y un poco más
+El barrio queda sin luz cuando no estás
+Las caras cambian todas cuando vos llegás
+La noche te sigue mientras vos girás
+Acá todos mueren por verte fumar
+Tu magia está cerca y protege el lugar
+Ahí voy a querer estar
+Donde tu risa me saca a bailar
+Cada vez que yo no me encuentro en mí, eh
+Voy a vos para revivir, eh
+Yo voy a estar ahí, yeh
+Siempre podés venir
+Ella camina con su viajero
+Entre desastres y prisioneros
+Cuando el destino se pone austero
+Sale al rescate lo verdadero
+Ella camina con su viajero
+Entre desastres y prisioneros
+Cuando el destino se pone austero
+Sale al rescate lo verdadero
+Comprensión bendita, un par de dilemas
+Humanidad maldita, cuerpo que tirita
+La mirada que desvela, de esas que te hielan
+Gratitud finita, noches sin estrellas o con miles de ellas
+Alma dinamita, voces en la cripta
+Felicidad compleja, una espera escrita
+Angustias bellas, onomatopeya
+Río que se agita, una luz interna
+Fuego que habilita lágrima que quema
+Protección eterna, un amor que grita
+Cada vez que yo no me encuentro en mí, eh
+Voy a vos para revivir, eh
+Yo voy a estar ahí, yeh
+Siempre podés venir
+Ella camina con su viajero
+Entre desastres y prisioneros
+Cuando el destino se pone austero
+Sale al rescate lo verdadero
+Ella camina con su viajero
+Entre desastres y prisioneros
+Cuando el destino se pone austero
+Sale al rescate lo verdade
+    `
+  },
+  {
+    image: 'https://indiehoy.com/wp-content/uploads/2019/10/wos-caravana.jpg',
+    title: 'Melon vino',
+    artist: 'WOS',
+    album: 'Caravana',
+    url: '/assets/melon-vino-wos.mp3',
+    lyrics: `
+Estoy sentado esperando que se pase el rato
+Estas palabras se parecen a mi autorretrato
+Ey, hoy ya no quiero hablar
+Si las sensaciones que en serio cambiaron mi vida
+No creo que las pueda explicar
+Voy a amarte y a tocarte
+Solo te pido que no me apuñales
+Cuando abra mi cuerpo pa' darte un lugar
+Y el mar se va con mis secretos
+Los va a llevar marea adentro
+Siempre vuelvo con mi sombra (ah)
+Te invito a que la conozcas, ah
+Me revuelco con mi sombra (ah)
+Te pido que no la rompas
+Tengo estudio y un colchón, tengo amigos, un montón
+Tengo vino y un melón, ready para el vacilón, ey
+Tengo, gano, busco, voy, rompo todo lo que soy
+Tengo estudio y un colchón, tengo amigos, un montón
+Tengo vino y un melón, ready para el vacilón, ey
+Tengo, gano, busco, voy, rompo todo lo que soy, wah
+Estoy sentado, esperando que se pase el rato
+Estas palabras se parecen a mi autorretrato
+Ey, hoy ya no quiero hablar
+Si las sensaciones que en serio cambiaron mi vida
+No creo que las pueda explicar
+Voy a convidarte una parte de mí
+Para ir por ahí, vamo' en mi zepelín
+Si el mundo está roto que traigan otro, o mejor nos vamos to' nosotros
+No quiero vivir con sabor a poco, no quiero morir sin volverme loco
+Cuando miremos y no entendamos nada de todo lo que ahora vemos
+Voy a quedarme con vos al lado cuando todo se nos ponga feo
+Cuando esta vida rara y descarada se lleve puesto a algún amigo
+Cuando lo más común pierda sentido, siempre vas a poder venir conmigo
+Mmm-mmm, nuestra mirada es la fuerza más linda de todas
+Quiero esconderme en tu pecho, que nada me joda
+Dale, vámonos y perdámonos
+Tirame una magia y llevame en tu escoba
+Tengo estudio y un colchón, tengo amigos, un montón
+Tengo vino y un melón, ready para el vacilón, ey
+Tengo, gano, busco, voy, rompo todo lo que soy
+Tengo estudio y un colchón, tengo amigos, un montón
+Tengo vino y un melón, ready para el vacilón, ey
+Tengo, gano, busco, voy, rompo todo lo que soy, wah
+Tengo estudio y un colchón, tengo amigos, un montón
+Tengo vino y un melón, ready para el vacilón, ey
+Tengo, gano, busco, voy, rompo todo lo que soy
+    `
+  },
+]
