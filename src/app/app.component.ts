d4238bb36f83bc56347a87d2c1c064dc951ac065
Jhonatan Yaranga <jhoonatan_0912@hotmail.com>
2023-04-11 19:40:58 -0500

    class added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { data } from 'src/data';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'reproductor';
+  audioPlayer: any;
+  songs: any[] = data
+
+  currentSongIndex: number = 0;
+  isPlaying: boolean = false;
+
+  constructor() {
+    this.audioPlayer = new Audio();
+    this.audioPlayer.src = this.songs[this.currentSongIndex].url;
+  }
+
+  playSong() {
+    this.audioPlayer.play();
+    this.isPlaying = true;
+  }
+
+  pauseSong() {
+    this.audioPlayer.pause();
+    this.isPlaying = false;
+  }
+
+  nextSong() {
+    this.currentSongIndex = (this.currentSongIndex + 1) % this.songs.length;
+    this.audioPlayer.src = this.songs[this.currentSongIndex].url;
+    this.playSong();
+  }
+
+  previousSong() {
+    this.currentSongIndex = (this.currentSongIndex - 1) % this.songs.length;
+    this.audioPlayer.src = this.songs[this.currentSongIndex].url;
+    this.playSong();
+  }
+
+  togglePlayPause() {
+    if (this.isPlaying) {
+      this.pauseSong();
+    } else {
+      this.playSong();
+    }
+  }
 }
